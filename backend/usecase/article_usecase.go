3802a10887046a931ef8652d1ab05062e80802f5
tsunakit99 <tainosuke0926@gmail.com>
2025-04-27 23:56:11 +0900

    add: CRUDユースケース実装

--- backend/usecase/article_usecase.go
+++ backend/usecase/article_usecase.go
@@ -1,6 +1,8 @@
 package usecase
 
 import (
+	"strings"
+
 	"github.com/tsunakit99/yomu/domain/model"
 	"github.com/tsunakit99/yomu/domain/repository"
 )
@@ -8,6 +10,9 @@ import (
 type ArticleUsecase interface {
 	GetAllArticles() ([]*model.Article, error)
 	GetArticleBySlug(slug string) (*model.ArticleDetail, error)
+	CreateArticle(input *model.ArticleInput) error
+	UpdateArticle(slug string, input *model.ArticleInput) error
+	DeleteArticle(slug string) error
 }
 
 type articleUsecase struct {
@@ -26,4 +31,23 @@ func (u *articleUsecase) GetAllArticles() ([]*model.Article, error) {
 
 func (u *articleUsecase) GetArticleBySlug(slug string) (*model.ArticleDetail, error) {
 	return u.repo.GetBySlug(slug)
+}
+
+func (u *articleUsecase) CreateArticle(input *model.ArticleInput) error {
+	slug := generateSlug(input.Title)
+
+	return u.repo.Create(slug, input)
+}
+
+func (u *articleUsecase) UpdateArticle(slug string, input *model.ArticleInput) error {
+	return u.repo.Update(slug, input)
+}
+
+func (u *articleUsecase) DeleteArticle(slug string) error {
+	return u.repo.Delete(slug)
+}
+
+// 🔥 slug自動生成（超シンプル版）
+func generateSlug(title string) string {
+	return strings.ReplaceAll(strings.ToLower(title), " ", "-")
 }
