66785d83cc0a9c77c869763e2bc8c5b73f3b461d
tsunakit99 <tainosuke0926@gmail.com>
2025-04-27 23:55:38 +0900

    add: CRUD処理をするリポジトリインターフェース定義

--- backend/domain/repository/article.go
+++ backend/domain/repository/article.go
@@ -5,4 +5,7 @@ import "github.com/tsunakit99/yomu/domain/model"
 type ArticleRepository interface {
 	GetAll() ([]*model.Article, error)
 	GetBySlug(slug string) (*model.ArticleDetail, error)
+	Create(slug string, input *model.ArticleInput) error
+	Update(slug string, input *model.ArticleInput) error
+	Delete(slug string) error
 }
